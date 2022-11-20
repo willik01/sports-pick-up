2a422abfb7ec9664a7e7795178df601aeec5a2a0
Keith Williams (willik01) <me@keithwilliams.net>
2022-11-19 22:18:34 -0800

    Clean up unnecessary files, add profile page

---
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -0,0 +1,11 @@
+import NavBar from '../../components/NavBar/NavBar'
+
+export default function ProfilePage(user) {
+    const[ profile, setsProfile] = userState([])
+    
+  return (
+    <main className="profile-page">
+        console.log("i am in the profile page")
+    </main>
+  );
+}
