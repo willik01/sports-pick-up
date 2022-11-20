be8b84914b35424aad979d0c8bfcee0cbb6c6abc
Keith Williams (willik01) <me@keithwilliams.net>
2022-11-19 22:23:21 -0800

    fix crashing bug on ProfilePage

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,11 +1,13 @@
+import { useState } from 'react'
+
 import NavBar from '../../components/NavBar/NavBar'
 
 export default function ProfilePage(user) {
-    const[ profile, setsProfile] = userState([])
+  const[ profile, setProfile] = useState([])
     
   return (
     <main className="profile-page">
-        console.log("i am in the profile page")
+        {console.log("i am in the profile page")}
     </main>
   );
 }
